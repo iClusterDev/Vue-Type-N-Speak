88dcee4053a0a126b91897c2e96a78a51d682243
iClusterDev <cluster.coredev@gmail.com>
2018-10-02 21:30:35 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,126 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col s6 offset-s3">
+
+          <!-- Logo Component -->
+          <div class="logo center">
+            <h3>Logo</h3>
+          </div>
+
+          <!-- Form Component -->
+          <div class="row">
+            <form class="col s12">
+
+              <!-- input text -->
+              <div class="input-component">
+                <div class="input-field">
+                  <label for="input-text">Type Anything...</label>
+                  <textarea 
+                    id="input-text" 
+                    class="materialize-textarea"
+                    v-model="text">
+                  </textarea>
+                </div>
+              </div>
+
+              <!-- input rate -->
+              <div class="input-component">
+                <label for="input-rate">
+                  <div>Rate<span class="right">{{rate}}</span></div>
+                </label>
+                <input 
+                  id="input-rate" 
+                  type="range" 
+                  min="0.5"
+                  max="2"
+                  step="0.1"
+                  v-model="rate">
+              </div>
+
+              <!-- input pitch -->
+              <div class="input-component">
+                <label for="input-pitch">
+                  <div>Pitch<span class="right">{{pitch}}</span></div>
+                </label>
+                <input 
+                  id="input-pitch" 
+                  type="range" 
+                  min="0" 
+                  max="2"
+                  step="0.1"
+                  v-model="pitch">
+              </div>
+
+              <!-- input voice -->
+              <div class="input-component">
+                <div class="input-field">
+                  <select
+                    id="input-voice" 
+                    class="">
+                    <template v-if="voices">
+                      <option v-for="(voice, index) of voices"
+                        :key="index" 
+                        :value="index">{{voice.name}}</option>
+                    </template>
+                  </select>
+                  <label>Voices</label>
+                </div>
+              </div>
+
+              <!-- speak button -->
+              <div class="center">
+                <button class="waves-effect waves-light btn">Say It</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- <pre>{{text}}</pre>
+    <pre>{{rate}}</pre>
+    <pre>{{pitch}}</pre> -->
+    <!-- <pre>{{synth.speaking}}</pre> -->
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Materialize from "materialize-css/dist/js/materialize";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      synth: window.speechSynthesis,
+      text: null,
+      rate: 1,
+      pitch: 1,
+      voices: null
+    };
+  },
+  methods: {
+    getVoices() {
+      this.voices = this.synth.getVoices();
+    }
+  },
+  mounted() {
+    this.getVoices();
+    if (this.synth.onvoiceschanged !== undefined) {
+      this.synth.onvoiceschanged = this.getVoices;
+    }
+  },
+  updated() {
+    Materialize.AutoInit();
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.input-component {
+  padding-bottom: 1.5rem;
 }
 </style>
